34ab8f72d0c632b6ff6464525cf945268e98a432
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-04-23 17:43:25 +0200

    mail

---
+++ java script/mail.js
@@ -0,0 +1,13 @@
+// Chiedi all’utente la sua email per poi controllare che sia nella lista di chi può accedere.
+// Stampa un messaggio SOLO SE l'utente è in lista.
+
+// chiedi la mail all'utente
+const mail = ("davide@prova.it");
+console.log (mail);
+const userMail = prompt ("scrivi qui la mail");
+console.log (userMail)
+
+// fai comparire un messaggio se presente
+if (userMail == mail) {
+    alert ("you inn")
+}
